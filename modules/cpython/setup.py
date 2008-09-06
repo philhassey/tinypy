d32e84eff4b9c5ba0a20bd3c80b7d8473500c00a
ulfada <ulfada@c0c488f3-8e49-0410-9d99-171a865aaac7>
2008-09-06 21:21:43 +0000

    removing cpython from modules, since it's been moved to root

--- modules/cpython/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-setup(name = "tinypy",
-      version = "0.8",
-      description = "tinypy module for CPython",
-      author = "Denis Kasak",
-      author_email = "denis.kasak@gmail.com",
-      url = "http://www.tinypy.org/",
-      ext_modules = [Extension("tinypy", ["cpython.c"], define_macros = [('CPYTHON_MOD', None)])])
